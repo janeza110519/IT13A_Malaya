27b460f22d19222ba71b97ef2428408c8fbed61d
janeza110519 <janezamalaya4@gmail.com>
2025-04-25 11:40:43 +0800

    Add files via upload

---
+++ IT13A_Activities/src/FINAL/GUI/Login.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FINAL.GUI;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author User
+ */
+public class Login extends JFrame{
+    public Login(){
+        JTextField jTextFieldUsername =new JTextField();
+        JPasswordField jPasswordPassword =new JPasswordField(); 
+        JLabel jLabelUsername = new JLabel("Username");
+        JLabel jLabelPassword = new JLabel("Password");
+        JButton jButtonEnter =new JButton("Enter");
+        JButton jButtonCancel =new JButton("Cancel");
+        jLabelUsername.setForeground(Color.red);
+        
+        setLayout(new GridLayout (3, 2));
+        getContentPane().add(jLabelUsername);
+        getContentPane().add(jTextFieldUsername);
+        getContentPane().add(jLabelPassword);
+        getContentPane().add(jPasswordPassword);
+        getContentPane().add(jButtonEnter);
+        getContentPane().add(jButtonCancel);
+        
+        
+        
+        
+        setBounds(600, 300, 400, 250);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
---
+++ IT13A_Activities/src/FINAL/Java_Login_GUI.java
@@ -0,0 +1,19 @@
+
+package FINAL;
+
+import FINAL.GUI.Login;
+
+
+public class Java_Login_GUI {
+    
+    public static void main(String[] args){
+        new JFrameLogin();
+        
+        
+        
+            
+            
+        }
+        
+        
+    }
